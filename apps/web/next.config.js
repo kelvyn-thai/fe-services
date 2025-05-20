e944d13c2a863d39e3d9674ad3d913e6cd7b0787
jayce.thai <phat.tnh@carehealth.io>
2025-05-20 15:28:19 +0700

    Fix some small issues

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
-export default nextConfig;
+const config = {
+  reactStrictMode: true,
+};
+
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})(config);
